50c0bb3d9fa009d3ba79287a9e693a6c7a3db0bd
Wenhao Piao <wenhaopi@amazon.com>
2021-12-20 22:08:47 -0800

    Add docstrings

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,6 +1,30 @@
 class LinkedList:
     """
-    Put docstring here
+    It consists of 0 or more nodes and its head property reference the first node in the list.
+    If it is an empty list, the head is None.
+    Each node has value property and next property. The next property represent a node it points to.
+
+    methods:
+    - insert
+        - Arguments: value
+        - Returns: nothing
+        - Adds a new node with that value to the head of the list with an O(1) Time performance.
+    - includes
+        - Arguments: value
+        - Returns: Boolean
+        - Indicates whether that value exists as a Node's value somewhere within the list.
+    - to_string
+        - Arguments: none
+        - Returns: a string representing all the values in the Linked List, formatted as: "{ a } -> { b } -> { c } -> None"
+    - append
+        - arguments: new value
+        - adds a new node with the given value to the end of the list
+    - insert before
+        - arguments: value, new value
+        - adds a new node with the given new value immediately before the first node that has the value specified
+    - insert after
+        - arguments: value, new value
+        - adds a new node with the given new value immediately after the first node that has the value specified
     """
 
     def __init__(self, head=None):
@@ -31,6 +55,9 @@ class LinkedList:
         return result + "None"
 
 class Node:
+    """
+    Each node has value property and next property. The next property represent a node it points to.
+    """
     def __init__(self, value, next=None):
         self.value = value
         self.next = next
