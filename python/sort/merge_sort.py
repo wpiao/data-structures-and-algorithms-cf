6aeacad9378b5341c9a2e35d3545c9fd7fa698b5
Wenhao Piao <wenhaopiao520@gmail.com>
2022-03-03 21:20:03 -0800

    WIP merge sort

---
+++ python/sort/merge_sort.py
@@ -0,0 +1,22 @@
+def merge_sort(l):
+    n = len(l)
+    if n > 1:
+        mid = n / 2
+        left = l[0:mid]
+        right = l[mid:n]
+        merge_sort(left)
+        merge_sort(right)
+        merge(left, right, l)
+
+def merge(left, right, l):
+    i = j = k = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            l[k] = left[i]
+            i += 1
+        else:
+            l[k] = right[j]
+            j += 1
+        k += 1
+
+    # if i == len(left):
